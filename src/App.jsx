4df78151d4e67e20c0cffd7c234772db964a6728
Itsmridulop <mridulmishra2117@gmail.com>
2024-08-26 20:21:48 -0400

    Manage package size

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { DarkModeProvider } from "./context/DarkModeContext"
 import { Toaster } from "react-hot-toast"
+import { Suspense, lazy } from "react"
 
 import ProtectedRoute from './ui/ProtectedRoute'
-import GlobalStyle from './styles/GlobalStyle'
-import Booking from './pages/Booking'
-import Account from "./pages/Account"
-import Settings from "./pages/Settings"
-import CheckIn from './pages/CheckIn'
-import Users from "./pages/Users"
-import Cabins from "./pages/Cabins"
-import Bookings from "./pages/Bookings"
-import Dashboard from "./pages/Dashboard"
 import AppLayout from "./ui/AppLayout"
-import Login from "./pages/Login"
-import PageNotFound from "./pages/PageNotFound"
-import { DarkModeProvider } from "./context/DarkModeContext"
+import Spinner from "./ui/Spinner"
+
+const Booking = lazy(() => import('./pages/Booking'))
+const Account = lazy(() => import('./pages/Account'))
+const Settings = lazy(() => import('./pages/Settings'))
+const CheckIn = lazy(() => import('./pages/CheckIn'))
+const Users = lazy(() => import('./pages/Users'))
+const Cabins = lazy(() => import('./pages/Cabins'))
+const Bookings = lazy(() => import('./pages/Bookings'))
+const Dashboard = lazy(() => import('./pages/Dashboard'))
+const Login = lazy(() => import('./pages/Login'))
+const PageNotFound = lazy(() => import('./pages/PageNotFound'))
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -30,28 +31,29 @@ function App() {
   return (
     <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
-        <GlobalStyle />
-        <Router>
-          <Routes>
-            <Route element={
-              <ProtectedRoute>
-                <AppLayout />
-              </ProtectedRoute>
-            }>
-              <Route index element={<Navigate replace to='/dashboard' />} />
-              <Route exact path="/dashboard" element={<Dashboard />} />
-              <Route exact path="/bookings" element={<Bookings />} />
-              <Route exact path='/cabins' element={<Cabins />} />
-              <Route exact path="/users" element={<Users />} />
-              <Route exact path="/settings" element={<Settings />} />
-              <Route exact path="/account" element={<Account />} />
-              <Route exact path="/bookings/:bookingId" element={<Booking />} />
-              <Route exact path="/checkin/:bookingId" element={<CheckIn />} />
-            </Route>
-            <Route exact path="login" element={<Login />} />
-            <Route exact path="*" element={<PageNotFound />} />
-          </Routes>
-        </Router>
+        <Suspense fallback={<Spinner/>}>
+          <Router>
+            <Routes>
+              <Route element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }>
+                <Route index element={<Navigate replace to='/dashboard' />} />
+                <Route exact path="/dashboard" element={<Dashboard />} />
+                <Route exact path="/bookings" element={<Bookings />} />
+                <Route exact path='/cabins' element={<Cabins />} />
+                <Route exact path="/users" element={<Users />} />
+                <Route exact path="/settings" element={<Settings />} />
+                <Route exact path="/account" element={<Account />} />
+                <Route exact path="/bookings/:bookingId" element={<Booking />} />
+                <Route exact path="/checkin/:bookingId" element={<CheckIn />} />
+              </Route>
+              <Route exact path="login" element={<Login />} />
+              <Route exact path="*" element={<PageNotFound />} />
+            </Routes>
+          </Router>
+        </Suspense>
         <Toaster
           position="top-center"
           gutter={12}
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from 'react-error-boundary'
 
+import GlobalStyle from './styles/GlobalStyle'
 import ErrorFallback from './ui/ErrorFallback.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyle />
     <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => window.location.replace('/')}>
       <App />
     </ErrorBoundary>
